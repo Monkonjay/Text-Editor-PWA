7694d9b8efbe9403d3971ab128fffde95a2a660a
R Greene <greenbert81@gmail.com>
2022-09-27 12:38:30 -0700

    removed commented test code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -26,23 +26,3 @@ window.addEventListener('appinstalled', (event) => {
 
 });
 
-
-
-// const textHeader = document.getElementById('textHeader');
-
-// window.addEventListener('beforeinstallprompt', (event) => {
-//   event.preventDefault();
-//   installBtn.style.visibility = 'visible';
-//   textHeader.textContent = 'Click the button to install!';
-
-//   installBtn.addEventListener('click', () => {
-//     event.prompt();
-//     installBtn.setAttribute('disabled', true);
-//     installBtn.textContent = 'Installed!';
-//   });
-// });
-
-// window.addEventListener('appinstalled', (event) => {
-//   textHeader.textContent = 'Successfully installed!';
-//   console.log('👍', 'appinstalled', event);
-// });
